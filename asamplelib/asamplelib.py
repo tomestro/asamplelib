76abb047db77efb4f5a3e84194ac8e6e6a3ba4ca
tomestro <tomestro@gmx.net>
2020-04-09 03:15:34 +0200

    Adding sample code and test

--- asamplelib/asamplelib.py
+++ asamplelib/asamplelib.py
@@ -1,1 +1,37 @@
 """Main module."""
+# import sys
+
+
+def sample_func(value):
+    """
+    Sample function
+    """
+    return value+1
+
+
+def inc(x):
+    """
+    Increase
+    """
+    return x + 1
+
+
+def sum(a, b):
+    """
+    Sum
+    """
+    return a + b
+
+
+def mult(a, b):
+    """
+    Multiply
+    """
+    return a * b
+
+
+def square(x):
+    """
+    Square
+    """
+    return x * x
--- tests/test_asamplelib.py
+++ tests/test_asamplelib.py
@@ -22,3 +22,37 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+# import pytest
+# import asamplelib
+
+
+@pytest.fixture
+def sample_fixture():
+    """Setup test fixture"""
+    return 1
+
+
+def test_func(sample_fixture):
+    """Test the sample function"""
+    assert asamplelib.sample_func(1) == 2
+
+
+def test_negative(sample_fixture):
+    """Negative test of sample function"""
+    assert asamplelib.sample_func(-2) == -1
+
+
+def test_inc():
+    assert asamplelib.inc(1) == 2
+
+
+def test_sum():
+    assert asamplelib.sum(3, 4) == 7
+
+
+def test_mult():
+    assert asamplelib.mult(3, 4) == 12
+
+
+def test_square():
+    assert asamplelib.square(5) == 25
